d7fe0032acb86ea74767667f91a715a1878a1cee
Ibragim <ibrohim7477454@gmail.com>
2025-02-03 22:26:55 +0300

    Add Chapter 1

---
+++ Python/Chapter_1/1.10-1.16.py
@@ -0,0 +1,36 @@
+print('-' * 20, 1.17, '-' * 20)
+a, b, x, y = input("t = "), input("v = "), input("x = "), input("y = ")
+print(f"a) 5 кг          б) {a} 1             в) {x} {y}")
+print(f"   13 17             19 {b}               5 {y}")
+
+
+print('-' * 20, 1.16, '-' * 20)
+t, v, x, y = input("t = "), input("v = "), input("x = "), input("y = ")
+print(f"a) 5 10          б) 100 {t}          в) {x} 25")
+print(f"   7 см             1949 {v}             {x} {y}")
+
+print('-' * 20, 1.15, '-' * 20)
+a = input("A = ")
+b = input("B = ")
+c = input("C = ")
+print(f"{a} {b} {c}")
+
+
+print('-' * 20, 1.14, '-' * 20)
+a = input("A = ")
+b = input("B = ")
+c = input("C = ")
+print(f"{a}  {b}  {c}")
+
+print('-' * 20, 1.13, '-' * 20)
+a = int(input("Введите число "))
+print(f"Следующее за числом {a} число - {a + 1}.")
+print(f"Для числа {a} предыдущее число {a - 1}.")
+
+print('-' * 20, 1.12, '-' * 20)
+name = input("Введите имя ")
+print(f"Привет, {name}!")
+
+print('-' * 20, 1.11, '-' * 20)
+name_footbal_team = input("Введите название команды ")
+print(name_footbal_team, " - это чемпион")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 1400
+Chapter 1 - Displaying information on the screen
+Chapter 2 - Calculation by formulas
