386841a5de547689e87454b11aaa26ac60736cc8
Ibragim <ibrohim7477454@gmail.com>
2025-02-03 22:53:12 +0300

    Add Chapter 2, ex.2.1 - 2.10

---
+++ Python/Chapter_2/2.1-2.10.py
@@ -0,0 +1,69 @@
+import math 
+print('-' * 20, 2.10, '-' * 20)
+num_1 = 6
+num_2 = 54
+arithmetic_mean = num_1 + num_2 / 2
+geometric_mean = math.sqrt(num_1 + num_2)
+print(arithmetic_mean, geometric_mean)
+
+print('-' * 20, 2.9, '-' * 20)
+print("a)")
+x = int(input("x = "))
+y = int(input("y = "))
+res_a = 2 * x ** 3 - 3.44 * x * y + 2.3 * x ** 2 - 7.1 * y + 2
+
+print("б)")
+a = int(input("a = "))
+b = int(input("b = "))
+res_b = 3.14 * (a + b) ** 3 + 2.75 * b ** 2 - 12.7 * a - 4.1
+print(res_a, res_b)
+
+print('-' * 20, 2.8, '-' * 20)
+r = 3
+L = 2 * r * math.pi
+S = math.pi * r ** 2
+print(L, S)
+
+print('-' * 20, 2.7, '-' * 20)
+a = 5
+P = a * 2
+S = a * 3
+print(P, S)
+
+print('-' * 20, 2.6, '-' * 20)
+R = 6450
+
+print('-' * 20, 2.5, '-' * 20)
+r = 5
+D = r * 2
+print(D)
+
+print('-' * 20, 2.4, '-' * 20)
+a = 4
+P = a * 4
+print(P)
+
+print('-' * 20, 2.3, '-' * 20)
+print("a)")
+a = int(input("a = "))
+res_a = math.sqrt((2 * a + math.sin(3 * a)) / 3.56)
+print(res_a)
+
+print("б)")
+b = int(input("b = "))
+res_b = math.sin((3.2 + math.sqrt(1 + b)) / 5 * b)
+
+print('-' * 20, 2.2, '-' * 20)
+a = int(input("a = "))
+res_a = (a ** 2 + 10) / math.sqrt(a ** 2 + 1)
+print(res_a)
+
+print('-' * 20, 2.1, '-' * 20)
+print("a)")
+x = int(input("x = "))
+res_x = 17 * x ** 2 - 6 * x + 13
+print(res_x)
+print("б)")
+a = int(input("a = "))
+res_a = 3 * a ** 2 + 5 * a - 21
+print(res_a)
