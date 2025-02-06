92416ef39422c07cb5809acf935ce9e3fb41c5c7
Ibragim <ibrohim7477454@gmail.com>
2025-02-06 22:00:24 +0300

    Add Chapter 2, ex.2.31- 2.38

---
+++ Python/Chapter_2/2.31-2.38.py
@@ -0,0 +1,38 @@
+from math import sqrt, sin, degrees, atan
+
+print('-' * 30, "2.21 - last", '-' * 30)
+
+def caluclate_2_38(x1, y1):
+    pass
+
+def caluclate_2_37(x1, y1):
+    pass
+
+def caluclate_2_36(x1, y1):
+    pass
+
+def caluclate_2_35(x1, y1):
+    pass
+
+def caluclate_2_34(x1, y1):
+    pass
+
+def caluclate_2_33(x1, y1):
+    pass
+
+def caluclate_2_32(x1, y1):
+    pass
+
+def caluclate_2_31(kg_sweet, kg_coockys, kg_apple):
+    kg_sweet_cost = 4 
+    kg_coockys_cost = 5 
+    kg_apple_cost = 3
+    summ_price = (kg_sweet * kg_sweet_cost) + (kg_coockys * kg_coockys_cost) + (kg_apple * kg_apple_cost)
+    return summ_price
+
+
+area = caluclate_2_32(12, 3)
+print(f"\nResults for 2.31: Area = {area}")
+
+a = caluclate_2_31(6, 6, 6)
+print(f"\nResults for 2.20: Summ = {a}\n")
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 1400
 Chapter 1 - Displaying information on the screen
 Chapter 2 - Calculation by formulas
+Chapter 3 - Integer arithmetic
